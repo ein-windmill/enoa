1c5d9b57c33b2401adcc6bdb7c01aecb6cc41f70
ein <ein-windmill@outlook.com>
2018-05-03 16:01:07 +0800

    eson

---
+++ enoa-eson/src/main/java/io/enoa/eson/json/_Ja.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2018, enoa (ein.windmill@outlook.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.enoa.eson.json;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+abstract class _Ja<E> implements Collection<E>, _Json {
+
+  Collection<E> coll;
+
+  _Ja() {
+    this(new ArrayList<>());
+  }
+
+  _Ja(Collection<E> collection) {
+    this.coll = collection;
+  }
+
+  @Override
+  public int size() {
+    return this.coll.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return this.coll.isEmpty();
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    return this.coll.contains(o);
+  }
+
+  @Override
+  public Iterator<E> iterator() {
+    return this.coll.iterator();
+  }
+
+  @Override
+  public Object[] toArray() {
+    return this.coll.toArray();
+  }
+
+  @Override
+  public <T> T[] toArray(T[] a) {
+    return this.coll.toArray(a);
+  }
+
+  @Override
+  public boolean add(E e) {
+    return this.coll.add(e);
+  }
+
+  @Override
+  public boolean remove(Object o) {
+    return this.coll.remove(o);
+  }
+
+  @Override
+  public boolean containsAll(Collection<?> c) {
+    return this.coll.containsAll(c);
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends E> c) {
+    return this.coll.addAll(c);
+  }
+
+  @Override
+  public boolean removeAll(Collection<?> c) {
+    return this.coll.removeAll(c);
+  }
+
+  @Override
+  public boolean retainAll(Collection<?> c) {
+    return this.coll.retainAll(c);
+  }
+
+  @Override
+  public void clear() {
+    this.coll.clear();
+  }
+}
---
+++ enoa-eson/src/main/java/io/enoa/eson/json/_Jo.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2018, enoa (ein.windmill@outlook.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.enoa.eson.json;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+abstract class _Jo implements Map<String, Object>, _Json {
+
+  Map<String, Object> map;
+
+  _Jo() {
+    this(new HashMap<>());
+  }
+
+  _Jo(Map<String, Object> map) {
+    this.map = map;
+  }
+
+  @Override
+  public int size() {
+    return this.map.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return this.map.isEmpty();
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return this.map.containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    return this.map.containsValue(value);
+  }
+
+  @Override
+  public Object get(Object key) {
+    return this.map.get(key);
+  }
+
+  @Override
+  public Object put(String key, Object value) {
+    return this.map.put(key, value);
+  }
+
+  @Override
+  public Object remove(Object key) {
+    return this.map.remove(key);
+  }
+
+  @Override
+  public void putAll(Map<? extends String, ?> m) {
+    this.map.putAll(m);
+  }
+
+  @Override
+  public void clear() {
+    this.map.clear();
+  }
+
+  @Override
+  public Set<String> keySet() {
+    return this.map.keySet();
+  }
+
+  @Override
+  public Collection<Object> values() {
+    return this.map.values();
+  }
+
+  @Override
+  public Set<Entry<String, Object>> entrySet() {
+    return this.map.entrySet();
+  }
+}
