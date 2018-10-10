3afac8f7c5c5dbe5649080d830d4033eb81df585
fewensa <fewensa@enoa.io>
2018-10-10 17:15:59 +0800

    fix http chunk

--- enoa-http/src/main/java/io/enoa/http/provider/httphelper/http/resp/_HttpHelperChunkedResponse.java
+++ enoa-http/src/main/java/io/enoa/http/provider/httphelper/http/resp/_HttpHelperChunkedResponse.java
@@ -52,21 +52,64 @@ public class _HttpHelperChunkedResponse extends AbstractResponse {
         }
       }
 
+//      try (ByteArrayOutputStream swap = new ByteArrayOutputStream();
+//           ByteArrayOutputStream temp = new ByteArrayOutputStream()) {
+//        byte[] buff = new byte[2];
+//        int rc = 0;
+//        while ((rc = stream.read(buff, 0, 2)) > 0) {
+//          if (chunk.stopper() != null && chunk.stopper().stop())
+//            return HttpResponseBody.create(swap.toByteArray(), super.charset());
+//
+//          byte curr = buff[0];
+//
+//          if (curr == R || curr == N) {
+//            temp.write(buff, 0, rc);
+//            byte[] bytes = temp.toByteArray();
+//            chunk.runner().run(bytes);
+//            swap.write(bytes);
+//            temp.reset();
+//            continue;
+//          }
+//          temp.write(buff, 0, rc);
+//        }
+//        byte[] bytes = swap.toByteArray();
+//        return HttpResponseBody.create(bytes, super.charset());
+//      }
+
       try (ByteArrayOutputStream swap = new ByteArrayOutputStream();
            ByteArrayOutputStream temp = new ByteArrayOutputStream()) {
-        byte[] buff = new byte[2];
+        byte[] buff = new byte[1];
         int rc = 0;
-        while ((rc = stream.read(buff, 0, 2)) > 0) {
+        boolean needn = false;
+        while ((rc = stream.read(buff, 0, 1)) > 0) {
           if (chunk.stopper() != null && chunk.stopper().stop())
             return HttpResponseBody.create(swap.toByteArray(), super.charset());
-
           byte curr = buff[0];
 
-          if (curr == R || curr == N) {
+          // \r
+          if (needn) {
+            if (curr != N) {
+              byte[] bytes = temp.toByteArray();
+              chunk.runner().run(bytes);
+              swap.write(bytes, 0, bytes.length);
+              temp.reset();
+              temp.write(buff, 0, rc);
+              continue;
+            }
+          }
+          // \r || \r\n
+          if (curr == R) {
+            needn = true;
+            temp.write(buff, 0, rc);
+            continue;
+          }
+          // \n
+          if (curr == N) {
+            needn = false;
             temp.write(buff, 0, rc);
             byte[] bytes = temp.toByteArray();
             chunk.runner().run(bytes);
-            swap.write(bytes);
+            swap.write(bytes, 0, bytes.length);
             temp.reset();
             continue;
           }
